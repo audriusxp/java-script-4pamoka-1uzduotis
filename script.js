08853dd17ff14f71ecd895f76ed6624c36fdb3dd
audriusxp <palm@inbox.lt>
2022-06-13 14:55:25 +0300

    atlikti uzduotis

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 window.onload = function (){
-    
+    let x = 100;
+    let y = 123;
+    let z = -212;
+    console.log(skaiciusDaugiauSimta(x));
+    console.log(skaiciusDaugiauSimta(y));
+    console.log(skaiciusDaugiauSimta(z));
+
 }
+function skaiciusDaugiauSimta(skaicius)
+{
+ if (skaicius>100){
+     return true;
+ }
+ else {
+     return false;
+ }
+  
+     
+  
+}
+
